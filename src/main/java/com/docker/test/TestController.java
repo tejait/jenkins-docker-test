d02d01e68c93b9346e854ef6967b06e6c1efeb3b
tejait <teja.chintapalli246@gmail.com>
2025-12-12 12:02:08 +0530

    Updated TestController.java

--- src/main/java/com/docker/test/TestController.java
+++ src/main/java/com/docker/test/TestController.java
@@ -14,6 +14,7 @@ public class TestController {
     }
     @GetMapping("hi")
     public String hi() {
+        System.out.println("Hi endpoint was called");
         return "Hi from TestController";
     }
 }
